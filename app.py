abd4ee246a62e5ccfa6f4e30c66bb44ae6c0685f
Jordan <jordan.milne2@gmail.com>
2020-04-27 17:50:13 -0400

    finished north american app

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
 from flask import Flask, render_template, request, jsonify
 import pickle
 import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+
+df = pd.read_csv('cars1.csv')
+df = df[df.price > 100]
+df = df[df.year > 2000]
+df = df[df.odometer < 1000000]
+df['name'] = df['manufacturer'].str.title() + ' ' + df['model'].str.title()
+df['name']
+
+le = LabelEncoder()
+le.fit(df['name'])
+df['model'] = le.transform(df['name'])
+
+
+
+
+
 
 app = Flask(__name__, template_folder='templates')
 #-------- MODEL GOES HERE -----------#
@@ -18,22 +35,20 @@ def predict_price():
     args = request.form
     print(args)
     data = pd.DataFrame({
-        'Name': [args.get('Name')],
-        'Location': [args.get('Location')],
-        'Year': [args.get('Year')],
-        'Kilometers_Driven': [args.get('Kilometers_Driven')],
-        'Fuel_Type': [args.get('Fuel_Type')],
-        'Transmission': [args.get('Transmission')],
-        'Owner_Type': [args.get('Owner_Type')],
-        'Mileage': [args.get('Mileage')],
-        'Engine': [args.get('Engine')],
-        'Power': [args.get('Power')],
-        'Seats': [args.get('Seats')],
+        'year': [args.get('year')],
+        'model': [le.transform([args.get('name')])[0]],
+        'cylinders': [args.get('cylinders')],
+        'fuel': [args.get('fuel')],
+        'odometer': [args.get('odometer')],
+        'transmission': [args.get('transmission')],
+        'paint_color': [args.get('paint_color')],
+        'condition': [args.get('condition')],
     })
-    prediction = f'${round(float(pipe.predict(data)[0]),-1)} CAD'
+
+    prediction = f'${round(float(pipe.predict(data)[0]*1.4),-1)} CAD'
     predd = round(float(pipe.predict(data)[0]),-1)
 
-    return render_template('result.html', prediction=prediction) if (predd < 999999) and (predd > 0) else render_template('result.html', prediction="You have selected the wrong fuel type or transmission type for that model of car OR you have entered an unrealistic value")
+    return render_template('result.html', prediction=prediction)
 
 
 if __name__ == '__main__':
--- model.py
+++ model.py
@@ -5,73 +5,84 @@ import pandas as pd
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.linear_model import LinearRegression, Lasso, Ridge
 from sklearn_pandas import DataFrameMapper, CategoricalImputer, FunctionTransformer
-from sklearn.preprocessing import StandardScaler, LabelBinarizer, PolynomialFeatures
+from sklearn.preprocessing import StandardScaler, LabelBinarizer, PolynomialFeatures, LabelEncoder
 from sklearn.impute import SimpleImputer
 from sklearn.feature_selection import SelectPercentile
 from sklearn.pipeline import make_pipeline
 
 
+df = pd.read_csv('cars1.csv')
+# df.info()
+
+# Dropping fake prices
+df = df[df.price > 100]
+
+# Dropping cars older than 20yrs
+df = df[df.year > 2000]
+
+# Dropping cars with over 1,000,000 kms - most likely user added extra digit by accident
+df = df[df.odometer < 1000000]
+
+df['name'] = df['manufacturer'].str.title() + ' ' + df['model'].str.title()
+df['name']
 
 
-df = pd.read_csv('train-data.csv')
-df = df.dropna(subset = ["Power", "Mileage", "Engine", "Seats"])
-#  Had to drop these null bhp strings
-df = df[df.Power != 'null bhp']
+# Encode models
+le = LabelEncoder()
+le.fit(df['name'])
+df['model'] = le.transform(df['name'])
 
-
-# turn into CAD
-df['Price'] = df['Price']*0.018*100000
-
-# Function to remove units from numbers
-def remove_words(column):
-    return column.str.split(' ').str[0]
-
-df['Mileage'] = remove_words(df['Mileage']).astype(float)
-df['Engine'] = remove_words(df['Engine']).astype(float)
-df['Power'] = remove_words(df['Power']).astype(float)
-df['New_Price'] = remove_words(df['New_Price']).astype(float)
-
-# Train test Split
-target = 'Price'
+# Train Test SPlit
+target = 'price'
 y = df[target]
 X = df.drop(target, axis=1)
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
 
-
+# DataFrame Mapper
 mapper = DataFrameMapper([
-     ('Name',[CategoricalImputer(), LabelBinarizer()]),
-     ('Location',[CategoricalImputer(), LabelBinarizer()]),
-     (['Year'],[SimpleImputer(), StandardScaler()]),
-     (['Kilometers_Driven'],[SimpleImputer(), StandardScaler()]),
-     ('Fuel_Type',[CategoricalImputer(), LabelBinarizer()]),
-     ('Transmission',[CategoricalImputer(), LabelBinarizer()]),
-     ('Owner_Type',[CategoricalImputer(), LabelBinarizer()]),
-     (['Mileage'], [SimpleImputer(), StandardScaler()]),
-     (['Engine'], [SimpleImputer(), StandardScaler()]),
-     (['Power'], [SimpleImputer(), StandardScaler()]),
-     (['Seats'], [SimpleImputer(), StandardScaler()]),
-     # (['New_Price'], [SimpleImputer(), StandardScaler()]),
+#     ('region', LabelBinarizer()),
+    (['year'], StandardScaler()),
+    # ('manufacturer',[CategoricalImputer(), LabelBinarizer()]),
+    ('model', [CategoricalImputer()]),
+    ('cylinders', [CategoricalImputer(), LabelBinarizer()]),
+    ('fuel', [CategoricalImputer(), LabelBinarizer()]),
+    (['odometer'], [SimpleImputer(), StandardScaler()]),
+    # ('title_status', [CategoricalImputer(), LabelBinarizer()]),
+    ('transmission', [CategoricalImputer(), LabelBinarizer()]),
+    # (['vin'], StandardScaler()),
+    # ('type', [CategoricalImputer(), LabelBinarizer()]),
+    ('paint_color', [CategoricalImputer(), LabelBinarizer()]),
+    ('condition', [CategoricalImputer(), LabelBinarizer()]),
      ], df_out=True)
 
+Z_train = mapper.fit_transform(X_train)
+Z_test = mapper.transform(X_test)
 
-# SelectPercentile
-select = SelectPercentile(percentile=40)
 
-## GridSearchCV to find best params for the pipe
+
+# # GridSearchCV to find best params for the pipe
 # params = {
-#     'iterations': [400],
-#     'learning_rate': [0.1,0.5,0.9],
+#     'iterations': [100,500],
+#     'learning_rate': [0.1,0.3,0.7],
+#     'depth': [4, 10],
 # }
 # grid = GridSearchCV(model, params, cv=3, n_jobs=-1, verbose=1)
 # grid.fit(Z_train, y_train)
 # print(grid.best_score_)
 # print(grid.best_params_)
 
-# CatBoostRegressor using the best params found above^
-model = cb.CatBoostRegressor(iterations=400, learning_rate=0.5)
 
-# pipe and pickle
-pipe = make_pipeline(mapper, select, model)
-pipe.fit(X_train, y_train)
-pipe.score(X_test, y_test)
-pickle.dump(pipe, open('pipe.pkl', 'wb'))
+
+
+# CatBoostRegressor using the best params found above^
+
+model = cb.CatBoostRegressor(depth= 10, iterations= 500, learning_rate =0.3)
+model.fit(Z_train,y_train)
+print(model.score(Z_train,y_train))
+print(model.score(Z_test, y_test))
+
+# # pipe and pickle
+# pipe = make_pipeline(mapper, model)
+# pipe.fit(X_train, y_train)
+# pipe.score(X_test, y_test)
+# pickle.dump(pipe, open('pipe.pkl', 'wb'))
